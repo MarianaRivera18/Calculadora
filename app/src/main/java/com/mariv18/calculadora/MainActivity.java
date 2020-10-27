b83efcb1e84d8a16780010e2899e672e46230a36
MaRiv <marianarivera1885@gmail.com>
2020-10-26 22:13:51 -0600

    modificacion de diseño , lectura de los numeros en textview

--- app/src/main/java/com/mariv18/calculadora/MainActivity.java
+++ app/src/main/java/com/mariv18/calculadora/MainActivity.java
@@ -4,13 +4,113 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView btnmostrar;
+    private Button btn0;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button btn9;
+    private Button btnmenos;
+    private Button btnac;
+    private Button btnnose;
+    private Button btnporciento;
+    private Button btnentre;
+    private Button btnpor;
+    private Button getBtnmenos;
+    private Button btnmas;
+    private Button btnresultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnmostrar = findViewById(R.id.btnmostrar);
+
+        btn0 = findViewById(R.id.btn0);
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textanterior = btnmostrar.getText() .toString();
+                btnmostrar.setText(textanterior + "0");
+             btnmostrar.setText("0");
+            }
+        });
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("2");
+            }
+        });
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("2");
+            }
+        });
+        btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("3");
+            }
+        });
+        btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("4");
+            }
+        });
+        btn5 = findViewById(R.id.btn5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("5");
+            }
+        });
+        btn6 = findViewById(R.id.btn6);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("6");
+            }
+        });
+        btn7 = findViewById(R.id.btn7);
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("7");
+            }
+        });
+        btn8 = findViewById(R.id.btn8);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("8");
+            }
+        });
+        btn9 = findViewById(R.id.btn9);
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnmostrar.setText("9");
+            }
+        });
     }
 
 
